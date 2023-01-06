f627857cf89910c7b42994ab02a378042218ea39
Flying0101 <stephano@live.se>
2023-01-06 19:15:03 +0100

    testing nav-modal

---
+++ src/components/NavModal.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+import '../css/NavModal.css';
+
+
+
+
+function NavModal() {
+
+
+
+    return (
+        <section className="nav-modal">
+asdasdasdasdasd
+        </section>
+    )
+
+}
+
+export default NavModal
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,40 +3,57 @@ import React, { useState } from 'react';
 import '../css/Navbar.css';
 import { FaAlignJustify } from 'react-icons/fa';
 import { FaAlignRight } from 'react-icons/fa';
+import { FaTimes } from 'react-icons/fa';
 
-
- 
 
 function Navbar() {
 
     const [tgl, setShowTgl] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
     function handleClick() {
         setShowTgl(!tgl);
+        setShowModal(!showModal);
     }
 
     return (
-        <nav className="navbar">
-            <p className="logo"><strong id="logo-sm">SM-</strong>consulting</p>
+        <div>{!showModal ? (
+            <nav className="navbar">
+                <p className="logo"><strong id="logo-sm">SM-</strong>consulting</p>
 
-            <ul className="nav-ul">
-                <li className="nav-li">Contact</li>
-                <li className="nav-li">Experience</li>
-            </ul>
+                <ul className="nav-ul">
+                    <li className="nav-li">About</li>
+                    <li className="nav-li">Reviews</li>
+                    <li className="nav-li">Services</li>
+                    <li className="nav-li">Contact</li>
+                </ul>
 
-            <div className="resp-container">
+                <div className="resp-container">
 
-                {!tgl ? (
-                    <FaAlignJustify className="responsive-menu" onClick={() => handleClick()} />
+                    {!tgl ? (
+                        <FaAlignJustify className="responsive-menu" onClick={() => handleClick()} />
 
-                ) : (
-                    <FaAlignRight className="responsive-menu" onClick={() => handleClick()} />
+                    ) : (
+                        <FaAlignRight className="responsive-menu" onClick={() => handleClick()} />
 
-                )}
+                    )}
 
-            </div>
+                </div>
 
-        </nav>
+            </nav>
+
+        ) : (
+            <nav className="nav-modal">
+                <FaTimes className="modal-menu" onClick={() => handleClick()} />
+                <ul className="m-nav-ul">
+                    <li className="m-nav-li">About</li>
+                    <li className="m-nav-li">Reviews</li>
+                    <li className="m-nav-li">Services</li>
+                    <li className="m-nav-li">Contact</li>
+                </ul>
+            </nav>
+        )}</div>
+
     )
 
 }
